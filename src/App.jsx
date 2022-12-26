e6f7a4bfe90ea5338e5f70d517668f5dddad2f89
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-12-26 15:33:46 -0500

    Installed Tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-
-
-function App() {
-
+const App = () => {
   return (
-    <h1>Hello from React.js</h1>
-  )
-}
+    <>
+      <h1 className="text-lg text-red-600">Hello from React!!!</h1>
+    </>
+  );
+};
 
-export default App
+export default App;
