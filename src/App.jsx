5e62c2c04a11430dc00694352891d62c76bbca9a
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-12-26 16:22:55 -0500

    Saving changes before push

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
-const fetchData = async () => {
-  const options = {
-    method: "GET",
-    url: "https://planets-info-by-newbapi.p.rapidapi.com/api/v1/planet/list",
-    headers: {
-      "X-RapidAPI-Key": "fa16bbeba3msh659eb472112419ep1f7f7djsnd35911ebf66e",
-      "X-RapidAPI-Host": "planets-info-by-newbapi.p.rapidapi.com",
-    },
+const App = () => {
+  const [planetsInfo, setPlanetsInfo] = useState([]);
+
+  const fetchData = async () => {
+    const options = {
+      method: "GET",
+      url: "https://planets-info-by-newbapi.p.rapidapi.com/api/v1/planet/list",
+      headers: {
+        "X-RapidAPI-Key": "fa16bbeba3msh659eb472112419ep1f7f7djsnd35911ebf66e",
+        "X-RapidAPI-Host": "planets-info-by-newbapi.p.rapidapi.com",
+      },
+    };
+  
+    try {
+      const res = await axios.request(options);
+      console.log(res.data);
+      setPlanetsInfo(res.data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  try {
-    const res = await axios.request(options);
-    console.log(res.data);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const App = () => {
   useEffect(() => {
     fetchData();
   }, []);
