76192fde0513defe80f6bb66ff05713cf15dc3ed
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-12-26 21:19:52 -0500

    Installed and added React Router

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,9 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
-  const [planetsInfo, setPlanetsInfo] = useState([]);
-
-  const fetchData = async () => {
-    const options = {
-      method: "GET",
-      url: "https://planets-info-by-newbapi.p.rapidapi.com/api/v1/planet/list",
-      headers: {
-        "X-RapidAPI-Key": "fa16bbeba3msh659eb472112419ep1f7f7djsnd35911ebf66e",
-        "X-RapidAPI-Host": "planets-info-by-newbapi.p.rapidapi.com",
-      },
-    };
-  
-    try {
-      const res = await axios.request(options);
-      console.log(res.data);
-      setPlanetsInfo(res.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   return (
-    <>
-      <h1 className="text-lg text-red-600">Hello from React!!!</h1>
-    </>
+    <div>...</div>
   );
 };
 
