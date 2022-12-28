240070fc2835d98c131206a58bf8297aad9b8163
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-12-28 18:23:09 -0500

    Finished implementation

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -9,22 +9,28 @@ const Checkout = () => {
   return (
     <div className="p-4">
       <h1 className="my-4 font-semibold text-3xl">Checkout</h1>
-      <div>
-        {console.log("cart products:", cartProducts)}
-        {cartProducts.map((product) => {
-          return (
-            <div key={product.id} id={product.id}>
-              <div>
-                <img src={product.image} alt={product.name} />
+      <div className="grid grid-cols-2">
+        <div>
+          {cartProducts.map((product) => {
+            return (
+              <div key={product.id} id={product.id} className="flex gap-4 my-8">
+                <div className="w-28">
+                  <img src={product.image} alt={product.name} />
+                </div>
+                <div className="flex flex-col justify-between">
+                  <div>
+                    <span className="block">{product.name}</span>
+                    <span className="block">Price: ${product.price}</span>
+                  </div>
+                  <span className="block">Quantity: {product.quantity}</span>
+                </div>
               </div>
-              <span className="block">{product.name}</span>
-              <span className="block">{product.price}</span>
-            </div>
-          );
-        })}
-      </div>
-      <div>
-        <strong>Total price: {totalPrice}</strong>
+            );
+          })}
+        </div>
+        <div>
+          <strong className="block text-center">Total price: ${totalPrice.toFixed(2)}</strong>
+        </div>
       </div>
     </div>
   );
